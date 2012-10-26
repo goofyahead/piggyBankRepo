490be16e53e8704b4dc37ab4c810ac2679ebd6ec
jonatantierno <jta.javato@gmail.com>
2012-10-27 00:41:43 +0200

    empty pig in gallery

--- src/es/finnapps/piggybank/activities/BrowsePigsActivity.java
+++ src/es/finnapps/piggybank/activities/BrowsePigsActivity.java
@@ -31,117 +31,123 @@ import es.finnapps.piggybank.sharedprefs.PiggyBankPreferences;
 
 public class BrowsePigsActivity extends RoboActivity implements OnItemSelectedListener, OnClickListener {
 
-	@InjectView(R.id.gallery)
-	private Gallery mGallery;
-	@InjectView(R.id.piggy_name_textview)
-	private TextView mPiggyNameTextView;
-	@InjectView(R.id.coin1)
-	private ImageButton mCoin1;
-	@InjectView(R.id.coin2)
-	private ImageButton mCoin2;
-	@InjectView(R.id.coin20)
-	private ImageButton mCoin20;
-	@InjectView(R.id.coin50)
-	private ImageButton mCoin50;
-	@InjectView(R.id.coin100)
-	private ImageButton mCoin100;
-	@InjectView(R.id.coin200)
-	private ImageButton mCoin200;
+    @InjectView(R.id.gallery)
+    private Gallery mGallery;
+    @InjectView(R.id.piggy_name_textview)
+    private TextView mPiggyNameTextView;
+    @InjectView(R.id.coin1)
+    private ImageButton mCoin1;
+    @InjectView(R.id.coin2)
+    private ImageButton mCoin2;
+    @InjectView(R.id.coin20)
+    private ImageButton mCoin20;
+    @InjectView(R.id.coin50)
+    private ImageButton mCoin50;
+    @InjectView(R.id.coin100)
+    private ImageButton mCoin100;
+    @InjectView(R.id.coin200)
+    private ImageButton mCoin200;
 
-	@Inject
-	private PiggyApiInterface mApi;
-	@Inject
-	private PiggyBankPreferences mPreferences;
+    @Inject
+    private PiggyApiInterface mApi;
+    @Inject
+    private PiggyBankPreferences mPreferences;
 
-	private List<Piggy> mPiggies;
-	private List<View> mPiggyViews;
+    private List<Piggy> mPiggies;
+    private List<View> mPiggyViews;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.browse_pigs_activity);
-		
-		mGallery.setOnItemSelectedListener(this);
-		
-		mCoin1.setOnClickListener(this);
-		mCoin2.setOnClickListener(this);
-		mCoin20.setOnClickListener(this);
-		mCoin50.setOnClickListener(this);
-		mCoin100.setOnClickListener(this);
-		mCoin200.setOnClickListener(this);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse_pigs_activity);
 
-		new AsyncTask<Void, Void, Void>() {
+        mGallery.setOnItemSelectedListener(this);
 
-			@Override
-			protected Void doInBackground(Void... params) {
-				mPiggies = mApi.getSharedPiggys(mPreferences.getUserPhone());
-				mPiggyViews = new ArrayList<View>();
+        mCoin1.setOnClickListener(this);
+        mCoin2.setOnClickListener(this);
+        mCoin20.setOnClickListener(this);
+        mCoin50.setOnClickListener(this);
+        mCoin100.setOnClickListener(this);
+        mCoin200.setOnClickListener(this);
 
-				for (int i = 0; i < mPiggies.size(); i++) {
-					ImageView view = new ImageView(BrowsePigsActivity.this);
-					view.setImageResource(R.drawable.cerdo_galeria);
-					mPiggyViews.add(view);
-				}
-				return null;
-			}
-			
-			protected void onPostExecute(Void result) {
-				mGallery.setAdapter(new PiggySpinner());
-			};
+        new AsyncTask<Void, Void, Void>() {
 
-		}.execute();
-	}
+            @Override
+            protected Void doInBackground(Void... params) {
+                mPiggies = mApi.getSharedPiggys(mPreferences.getUserPhone());
+                mPiggyViews = new ArrayList<View>();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_browse_pigs_activicity, menu);
-		return true;
-	}
+                for (int i = 0; i < mPiggies.size(); i++) {
+                    ImageView view = new ImageView(BrowsePigsActivity.this);
+                    view.setImageResource(R.drawable.cerdo_galeria);
+                    mPiggyViews.add(view);
+                }
+                // Add empty piggy
+                ImageView emptyPiggy = new ImageView(BrowsePigsActivity.this);
+                emptyPiggy.setImageResource(R.drawable.cerdo_galeria);
+                mPiggyViews.add(emptyPiggy);
+                
+                return null;
+            }
 
-	class PiggySpinner extends BaseAdapter {
-		public int getCount() {
-			return mPiggies.size();
-		}
+            protected void onPostExecute(Void result) {
+                mGallery.setAdapter(new PiggySpinner());
+            };
 
-		public Object getItem(int position) {
-			return mPiggies.get(position);
-		}
+        }.execute();
+    }
 
-		public long getItemId(int position) {
-			return mPiggies.get(position).getId();
-		}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_browse_pigs_activicity, menu);
+        return true;
+    }
 
-		public View getView(int position, View convertView, ViewGroup parent) {
-			return mPiggyViews.get(position);
-		}
+    class PiggySpinner extends BaseAdapter {
+        public int getCount() {
+            return mPiggies.size();
+        }
 
-	}
+        public Object getItem(int position) {
+            return mPiggies.get(position);
+        }
+
+        public long getItemId(int position) {
+            return mPiggies.get(position).getId();
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return mPiggyViews.get(position);
+        }
+
+    }
 
     public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
-         mPiggyNameTextView.setText(mPiggies.get(position).getName());
-        
+        mPiggyNameTextView.setText(mPiggies.get(position).getName());
+
     }
 
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
-        
+
     }
 
     public void onClick(View v) {
         Animation scaleAnim = AnimationUtils.loadAnimation(this, R.anim.coin_click);
         v.startAnimation(scaleAnim);
-        
-        new AsyncTask<Void, Void, Void>(){
+
+        new AsyncTask<Void, Void, Void>() {
 
             @Override
             protected Void doInBackground(Void... params) {
                 return null;
             }
+
             protected void onPostExecute(Void result) {
-                
+
             };
-            
+
         }.execute();
-        
+
     }
 }
