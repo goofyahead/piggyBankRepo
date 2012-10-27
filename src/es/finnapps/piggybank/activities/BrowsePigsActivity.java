979c5b0fa5e577156530d70d9b2833df02e62821
jonatantierno <jta.javato@gmail.com>
2012-10-27 05:50:11 +0200

    merged manually

--- src/es/finnapps/piggybank/activities/BrowsePigsActivity.java
+++ src/es/finnapps/piggybank/activities/BrowsePigsActivity.java
@@ -1,14 +1,21 @@
-<<<<<<< HEAD
 package es.finnapps.piggybank.activities;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+import android.nfc.NfcEvent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -24,6 +31,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.inject.Inject;
 
@@ -33,7 +41,7 @@ import es.finnapps.piggybank.model.Piggy;
 import es.finnapps.piggybank.piggyapi.PiggyApiInterface;
 import es.finnapps.piggybank.sharedprefs.PiggyBankPreferences;
 
-public class BrowsePigsActivity extends RoboActivity implements OnItemSelectedListener, OnClickListener {
+public class BrowsePigsActivity extends RoboActivity implements CreateNdefMessageCallback,OnItemSelectedListener, OnClickListener {
 
     @InjectView(R.id.gallery)
     private Gallery mGallery;
@@ -53,7 +61,9 @@ public class BrowsePigsActivity extends RoboActivity implements OnItemSelectedLi
     private ImageButton mCoin200;
     @InjectView(R.id.progressbar)
     private ProgressBar mProgressBar;
-
+    private NfcAdapter mNfcAdapter;
+    private Piggy currentPiggy = null;
+    
     @Inject
     private PiggyApiInterface mPiggyApi;
     @Inject
@@ -69,6 +79,13 @@ public class BrowsePigsActivity extends RoboActivity implements OnItemSelectedLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browse_pigs_activity);
 
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        
         mGallery.setOnItemSelectedListener(this);
 
         mCoin1.setOnClickListener(this);
@@ -122,6 +139,8 @@ public class BrowsePigsActivity extends RoboActivity implements OnItemSelectedLi
             };
 
         }.execute();
+
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
     }
 
     @Override
@@ -150,12 +169,12 @@ public class BrowsePigsActivity extends RoboActivity implements OnItemSelectedLi
     }
 
     public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
+        currentPiggy = mPiggies.get(position);
         if (view == mEmptyPiggyView){
             mPiggyNameTextView.setText(getString(R.string.new_piggy));
            return; 
         }
         mPiggyNameTextView.setText(mPiggies.get(position).getName());
-
     }
 
     public void onNothingSelected(AdapterView<?> arg0) {
@@ -208,191 +227,7 @@ public class BrowsePigsActivity extends RoboActivity implements OnItemSelectedLi
 
         }.execute();
     }
-}
-=======
-package es.finnapps.piggybank.activities;
-
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
-import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectView;
-import android.content.Intent;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.NfcEvent;
-import android.nfc.NfcAdapter.CreateNdefMessageCallback;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.BaseAdapter;
-import android.widget.Gallery;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.inject.Inject;
-
-import es.finnapps.piggybank.R;
-import es.finnapps.piggybank.model.Piggy;
-import es.finnapps.piggybank.piggyapi.PiggyApiInterface;
-import es.finnapps.piggybank.sharedprefs.PiggyBankPreferences;
-
-public class BrowsePigsActivity extends RoboActivity implements CreateNdefMessageCallback, OnItemSelectedListener,
-        OnClickListener {
-
-    @InjectView(R.id.gallery)
-    private Gallery mGallery;
-    @InjectView(R.id.piggy_name_textview)
-    private TextView mPiggyNameTextView;
-    @InjectView(R.id.coin1)
-    private ImageButton mCoin1;
-    @InjectView(R.id.coin2)
-    private ImageButton mCoin2;
-    @InjectView(R.id.coin20)
-    private ImageButton mCoin20;
-    @InjectView(R.id.coin50)
-    private ImageButton mCoin50;
-    @InjectView(R.id.coin100)
-    private ImageButton mCoin100;
-    @InjectView(R.id.coin200)
-    private ImageButton mCoin200;
-    private NfcAdapter mNfcAdapter;
-
-    @Inject
-    private PiggyApiInterface mApi;
-    @Inject
-    private PiggyBankPreferences mPreferences;
-    private ImageView mEmptyPiggyView;
-    private List<Piggy> mPiggies;
-    private List<View> mPiggyViews;
-    private Piggy currentPiggy = null;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.browse_pigs_activity);
-
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if (mNfcAdapter == null) {
-            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-
-        mGallery.setOnItemSelectedListener(this);
-
-        mCoin1.setOnClickListener(this);
-        mCoin2.setOnClickListener(this);
-        mCoin20.setOnClickListener(this);
-        mCoin50.setOnClickListener(this);
-        mCoin100.setOnClickListener(this);
-        mCoin200.setOnClickListener(this);
-
-        // Empty piggy, for creating new piggy
-        mEmptyPiggyView = new ImageView(BrowsePigsActivity.this);
-        mEmptyPiggyView.setImageResource(R.drawable.cerdo_galeria);
-        mEmptyPiggyView.setOnClickListener(BrowsePigsActivity.this);
-
-        new AsyncTask<Void, Void, Void>() {
-
-            @Override
-            protected Void doInBackground(Void... params) {
-                mPiggies = new ArrayList<Piggy>();
-                mPiggies.addAll(mApi.getSharedPiggys(mPreferences.getUserPhone()));
-                mPiggyViews = new ArrayList<View>();
-
-                for (int i = 0; i < mPiggies.size(); i++) {
-                    ImageView view = new ImageView(BrowsePigsActivity.this);
-                    view.setImageResource(R.drawable.cerdo_galeria);
-                    mPiggyViews.add(view);
-                }
-                // Add empty piggy
-                mPiggyViews.add(mEmptyPiggyView);
-
-                return null;
-            }
-
-            protected void onPostExecute(Void result) {
-                mGallery.setAdapter(new PiggySpinner());
-            };
-
-        }.execute();
-        mNfcAdapter.setNdefPushMessageCallback(this, this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_browse_pigs_activicity, menu);
-        return true;
-    }
-
-    class PiggySpinner extends BaseAdapter {
-        public int getCount() {
-            return mPiggies.size();
-        }
-
-        public Object getItem(int position) {
-            return mPiggies.get(position);
-        }
-
-        public long getItemId(int position) {
-            return mPiggies.get(position).getId();
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            return mPiggyViews.get(position);
-        }
-
-    }
-
-    public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
-        if (view == mEmptyPiggyView) {
-
-        }
-        mPiggyNameTextView.setText(mPiggies.get(position).getName());
-        currentPiggy = mPiggies.get(position);
-    }
-
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void onClick(View v) {
-        Animation scaleAnim = AnimationUtils.loadAnimation(this, R.anim.coin_click);
-        v.startAnimation(scaleAnim);
-
-        if (v == mEmptyPiggyView) {
-            startActivity(new Intent(BrowsePigsActivity.this, CreatePiggyActivity.class));
-        }
-        new AsyncTask<Void, Void, Void>() {
-
-            @Override
-            protected Void doInBackground(Void... params) {
-                return null;
-            }
-
-            protected void onPostExecute(Void result) {
-
-            };
-
-        }.execute();
-
-    }
-
+    
     public NdefMessage createNdefMessage(NfcEvent event) {
 
         NdefMessage msg = new NdefMessage(
@@ -457,4 +292,3 @@ public class BrowsePigsActivity extends RoboActivity implements CreateNdefMessag
         return true;
     }
 }
->>>>>>> origin/develop
